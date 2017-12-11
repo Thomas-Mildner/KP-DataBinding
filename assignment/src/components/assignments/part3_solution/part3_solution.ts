6f3a2385ff9723b456862a848f50ef5c387bda82
Peter Kurfer <peter.kurfer@googlemail.com>
2017-12-11 11:20:21 +0100

    Completed solution for assignment 1 and moved assignment 3 to 2

--- assignment/src/components/assignments/part3_solution/part3_solution.ts
+++ assignment/src/components/assignments/part3_solution/part3_solution.ts
@@ -1,42 +1,16 @@
 import {Component, Vue} from 'vue-property-decorator';
-import axios, {AxiosInstance} from 'axios';
 
 import './part3.scss';
-import {FormattableJoke, Joke, JokesArrayResponse} from './models/Joke';
 
 @Component({
   template: require('./part3.html')
 })
 export class AssignmentPart3SolutionComponent extends Vue {
-
-  private url = 'https://icndb.kns-it.de/api/v1/jokes?page_size=';
-  private axios: AxiosInstance;
-
-  items: Joke[] = [];
-  pageSizes: number[] = [1, 5, 10, 20, 50, 100, 200, 500];
-  currentPageSize: number = 1;
-  firstName: string = 'Chuck';
-  lastName: string = 'Norris';
-
   constructor() {
     super();
-    this.axios = axios;
   }
 
-  async mounted() {
-    await this.loadItems();
-  }
+  mounted() {
 
-  async updatePageSize(newSize: number) {
-    this.currentPageSize = newSize;
-    await this.loadItems();
-  }
-
-  private async loadItems() {
-    let temp: JokesArrayResponse = (await this.axios.get(this.url + this.currentPageSize)).data;
-    this.items = [];
-    for (let j of temp.jokes) {
-      this.items.push(new FormattableJoke(j));
-    }
   }
 }
