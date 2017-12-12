36f0a5eceb71f77f13789e2dc68eb990300f224a
Peter Kurfer <peter.kurfer@googlemail.com>
2017-12-12 10:17:39 +0100

    Updated docs to fit graphics

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-pandoc assets/templates/header.md  LectureNotes.md -o assets/documents/LectureNotes.pdf --template assets/templates/eisvogel.tex --listings --pdf-engine=xelatex
+cp LectureNotes.md LectureNotes_PDF.md
+
+sed -i -e "s/mvcConcept.png)/mvcConcept.png){width=50%}/g" LectureNotes_PDF.md
+sed -i -e "s/validation.png)/validation.png){height=60%}/g" LectureNotes_PDF.md
+
+pandoc assets/templates/header.md  LectureNotes_PDF.md -o assets/documents/LectureNotes.pdf --template assets/templates/eisvogel.tex --listings --pdf-engine=xelatex
 pandoc AssignmentSpec.md -o assets/documents/AssignmentSpec.pdf --template assets/templates/eisvogel.tex --listings --pdf-engine=xelatex
+
+rm -f LectureNotes_PDF.md
